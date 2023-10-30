4059aaf6cce6038e729ba0c336993a55d04c515f
Romain Fromi <romain.fromi@gmail.com>
2023-10-30 14:27:31 +0100

    Reroll dices

--- rules/src/rules/ActivePlayerTurn.ts
+++ rules/src/rules/ActivePlayerTurn.ts
@@ -1,6 +1,7 @@
-import { MaterialMove, PlayerTurnRule } from '@gamepark/rules-api'
+import { isMoveItem, isRoll, ItemMove, MaterialMove, PlayerTurnRule } from '@gamepark/rules-api'
 import { LocationType } from '../material/LocationType'
 import { MaterialType } from '../material/MaterialType'
+import { Memory } from './Memory'
 
 export class ActivePlayerTurn extends PlayerTurnRule {
   onRuleStart(): MaterialMove[] {
@@ -8,7 +9,31 @@ export class ActivePlayerTurn extends PlayerTurnRule {
   }
 
   getPlayerMoves() {
+    const discardedDice = this.remind(Memory.DiscardedDice)
+    if (discardedDice !== undefined) {
+      return this.rerollDice
+    }
+    return this.discardDice
+  }
+
+  get discardDice() {
     return this.material(MaterialType.Dice).location(LocationType.PlayerDices).player(this.player)
       .moveItems(item => ({ type: LocationType.DiscardTile, parent: 0, rotation: item.location.rotation }))
+  }
+
+  get rerollDice() {
+    return this.material(MaterialType.Dice).location(LocationType.PlayerDices).player(this.player).rollItems()
+  }
+
+  afterItemMove(move: ItemMove): MaterialMove[] {
+    switch (move.itemType) {
+      case MaterialType.Dice:
+        if (isMoveItem(move) && move.location.type === LocationType.DiscardTile) {
+          this.memorize(Memory.DiscardedDice, move.itemIndex)
+        } else if (isRoll(move)) {
+          this.forget(Memory.DiscardedDice)
+        }
+    }
+    return []
   }
 }
