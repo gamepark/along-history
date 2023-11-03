58c3fc1b374aea4ac8c7d7c6b8e34a3b6ddb1e02
Romain Fromi <romain.fromi@gmail.com>
2023-11-03 17:19:21 +0100

    Start a new round

--- rules/src/rules/UniversalResourceRule.ts
+++ rules/src/rules/UniversalResourceRule.ts
@@ -13,7 +13,26 @@ export class UniversalResourceRule extends PlayerTurnRule {
       moves.push(this.material(MaterialType.UniversalResource).location(LocationType.UniversalResourceStock)
         .moveItem({ type: LocationType.PlayerUniversalResource, player: this.player }, 1))
     }
+    if (this.material(MaterialType.DiscardTile).getItem()?.location.player === this.nextPlayer) {
+      return this.endRound
+    }
     moves.push(this.rules().startPlayerTurn(RuleId.Upkeep, this.nextPlayer))
     return moves
+  }
+
+  get endRound() {
+    if (this.ageIsOver) {
+      return [this.rules().endGame()]
+    } else {
+      const nextActivePlayer = this.game.players[(this.game.players.indexOf(this.player) + 2) % this.game.players.length]
+      return [
+        this.material(MaterialType.DiscardTile).moveItem({ type: LocationType.PlayerDiscardTile, player: nextActivePlayer }),
+        this.rules().startPlayerTurn(RuleId.Upkeep, nextActivePlayer)
+      ]
+    }
+  }
+
+  get ageIsOver() {
+    return false // TODO
   }
 }
