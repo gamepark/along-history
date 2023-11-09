62a6b9c16888017e26b61e86c3e08a5e6c875270
mizutismask <severinek@gmail.com>
2023-11-09 16:30:56 +0100

    chore: reformat

--- app/src/locators/CivilisationAreaLocator.ts
+++ app/src/locators/CivilisationAreaLocator.ts
@@ -1,86 +1,87 @@
-import { LocationType } from '@gamepark/along-history/material/LocationType';
-import { MaterialType } from '@gamepark/along-history/material/MaterialType';
-import { getRelativePlayerIndex, ItemContext, ItemLocator, LocationDescription, MaterialContext } from '@gamepark/react-game';
-import { Coordinates, MaterialItem } from '@gamepark/rules-api';
-import { boardDescription } from '../material/BoardDescription';
-import { cardDescription } from '../material/CardDescription';
-import { getPlayerRotation } from './getPlayerRotation';
+import { LocationType } from '@gamepark/along-history/material/LocationType'
+import { MaterialType } from '@gamepark/along-history/material/MaterialType'
+import { getRelativePlayerIndex, ItemContext, ItemLocator, LocationDescription, MaterialContext } from '@gamepark/react-game'
+import { Coordinates, MaterialItem } from '@gamepark/rules-api'
+import { boardDescription } from '../material/BoardDescription'
+import { cardDescription } from '../material/CardDescription'
+import { getPlayerRotation } from './getPlayerRotation'
 
 class CivilisationAreaLocator extends ItemLocator {
-	locationDescription = new CivilisationAreaDescription();
+    locationDescription = new CivilisationAreaDescription()
 
-	/**
-	 * Calculate absolute position of the elements inside this area, integrating spaces between cards and decay shifts.
-	 * @param item
-	 * @param context
-	 * @returns
-	 */
-	getPosition(item: MaterialItem, context: ItemContext): Coordinates {
-		const previousColumnsContent: Array<number> = [];
-		[...Array(item.location.x).keys()].forEach((column) => {
-			previousColumnsContent[column] = context.rules
-				.material(MaterialType.Card)
-				.player(item.location.player)
-				.locationId(item.location.id)
-				.location((loc) => loc.type === item.location.type && loc.x === column).length;
-		});
+    /**
+     * Calculate absolute position of the elements inside this area, integrating spaces between cards and decay shifts.
+     * @param item
+     * @param context
+     * @returns
+     */
+    getPosition(item: MaterialItem, context: ItemContext): Coordinates {
+        const previousColumnsContent: Array<number> = []
+        ;[...Array(item.location.x).keys()].forEach((column) => {
+            previousColumnsContent[column] = context.rules
+                .material(MaterialType.Card)
+                .player(item.location.player)
+                .locationId(item.location.id)
+                .location((loc) => loc.type === item.location.type && loc.x === column).length
+        })
 
-		const shiftDecay = 1;
-		const shiftCard = 1;
-		const playerIndex = getRelativePlayerIndex(context, item.location.player);
-		const previousShiftsCount = previousColumnsContent.reduce((acc, colCont) => acc + colCont - 1, 0);
-		let originX, originY;
-		switch (playerIndex) {
-			case 0:
-				originX = cardDescription.width / 2 - boardDescription.width / 2;
-				originY = boardDescription.height / 2 + cardDescription.height * 1.5 + 2;
-				return {
-					x:
-						originX +
-						cardDescription.width * previousColumnsContent.length + //all cards width total
-						shiftCard * previousColumnsContent.length + //shift between each card, total
-						shiftDecay * previousShiftsCount + //previous piles shifts
-						shiftDecay * item.location.z!, //this pile shift
-					y: originY,
-					z: 5 - item.location.z! * 0.1 - item.location.x! * 0.05,
-				};
-			default:
-				originX = boardDescription.width / 2 + cardDescription.height * 1.5 + 2;
-				originY = boardDescription.height / 2 - cardDescription.width / 2;
-				return {
-					x: originX,
-					y: originY -
-          cardDescription.width * previousColumnsContent.length - //all cards width total
-          shiftCard * previousColumnsContent.length - //shift between each card, total
-          shiftDecay * previousShiftsCount - //previous piles shifts
-          shiftDecay * item.location.z!, //this pile shift
-					z: 5 - item.location.z! * 0.1 - item.location.x! * 0.05,
-				};
-		}
-	}
+        const shiftDecay = 1
+        const shiftCard = 1
+        const playerIndex = getRelativePlayerIndex(context, item.location.player)
+        const previousShiftsCount = previousColumnsContent.reduce((acc, colCont) => acc + colCont - 1, 0)
+        let originX, originY
+        switch (playerIndex) {
+            case 0:
+                originX = cardDescription.width / 2 - boardDescription.width / 2
+                originY = boardDescription.height / 2 + cardDescription.height * 1.5 + 2
+                return {
+                    x:
+                        originX +
+                        cardDescription.width * previousColumnsContent.length + //all cards width total
+                        shiftCard * previousColumnsContent.length + //shift between each card, total
+                        shiftDecay * previousShiftsCount + //previous piles shifts
+                        shiftDecay * item.location.z!, //this pile shift
+                    y: originY,
+                    z: 5 - item.location.z! * 0.1 - item.location.x! * 0.05,
+                }
+            default:
+                originX = boardDescription.width / 2 + cardDescription.height * 1.5 + 2
+                originY = boardDescription.height / 2 - cardDescription.width / 2
+                return {
+                    x: originX,
+                    y:
+                        originY -
+                        cardDescription.width * previousColumnsContent.length - //all cards width total
+                        shiftCard * previousColumnsContent.length - //shift between each card, total
+                        shiftDecay * previousShiftsCount - //previous piles shifts
+                        shiftDecay * item.location.z!, //this pile shift
+                    z: 5 - item.location.z! * 0.1 - item.location.x! * 0.05,
+                }
+        }
+    }
 
-	getRotations(item: MaterialItem, context: ItemContext): string[] {
-		let rotation = getPlayerRotation(context, item.location.player);
-		if (item.location.rotation) {
-			rotation += 90;
-		}
-		return [`rotateZ(${rotation}deg)`];
-	}
+    getRotations(item: MaterialItem, context: ItemContext): string[] {
+        let rotation = getPlayerRotation(context, item.location.player)
+        if (item.location.rotation) {
+            rotation += 90
+        }
+        return [`rotateZ(${rotation}deg)`]
+    }
 }
 
 class CivilisationAreaDescription extends LocationDescription {
-	height = cardDescription.height + 1;
-	width = boardDescription.width;
-	borderRadius = cardDescription.borderRadius;
-	coordinates = {
-		x: 0,
-		y: boardDescription.height / 2 + cardDescription.height * 1.5 + 2,
-		z: 0,
-	};
+    height = cardDescription.height + 1
+    width = boardDescription.width
+    borderRadius = cardDescription.borderRadius
+    coordinates = {
+        x: 0,
+        y: boardDescription.height / 2 + cardDescription.height * 1.5 + 2,
+        z: 0,
+    }
 
-	getLocations({ player }: MaterialContext) {
-		return player !== undefined ? [{ type: LocationType.CivilisationArea, player }] : [];
-	}
+    getLocations({ player }: MaterialContext) {
+        return player !== undefined ? [{ type: LocationType.CivilisationArea, player }] : []
+    }
 }
 
-export const civilisationAreaLocator = new CivilisationAreaLocator();
+export const civilisationAreaLocator = new CivilisationAreaLocator()
--- rules/src/util/CivilisationAreaStrategy.ts
+++ rules/src/util/CivilisationAreaStrategy.ts
@@ -1,67 +1,67 @@
-import { LocationStrategy, Material, MaterialItem } from '@gamepark/rules-api';
+import { LocationStrategy, Material, MaterialItem } from '@gamepark/rules-api'
 
 /**
  * This strategy uses a consecutive sequence of numbers starting with 0 for items at the same location according to the axis, and allows several elements to be at the same position but with another sequence for depth (Z)
  */
 export class CivilisationAreaStrategy<P extends number = number, M extends number = number, L extends number = number> implements LocationStrategy<P, M, L> {
-	axis: 'x' | 'y' | 'z';
+    axis: 'x' | 'y' | 'z'
 
-	constructor(axis: 'x' | 'y' | 'z' = 'x') {
-		this.axis = axis;
-	}
+    constructor(axis: 'x' | 'y' | 'z' = 'x') {
+        this.axis = axis
+    }
 
-	/**
-	 *
-	 * @param material contains all other elements at the same place, before actually adding the item
-	 * @param item
-	 */
-	addItem(material: Material<P, M, L>, item: MaterialItem<P, L>): void {
-		const x = item.location[this.axis];
-		if (x === undefined) {
-			item.location[this.axis] = new Set(material.getItems().map((item) => item.location.x)).size;
-			item.location['z'] = 0;
-		}
+    /**
+     *
+     * @param material contains all other elements at the same place, before actually adding the item
+     * @param item
+     */
+    addItem(material: Material<P, M, L>, item: MaterialItem<P, L>): void {
+        const x = item.location[this.axis]
+        if (x === undefined) {
+            item.location[this.axis] = new Set(material.getItems().map((item) => item.location.x)).size
+            item.location['z'] = 0
+        }
 
-		if (material.location((loc) => loc.x === x).length !== 0) {
-			//we’re adding a card under the first one, which never moves. Under this first one, it’s a regular pile
-			//so this card becomes the first of the pile under, and everything in this pile needs to be shifted
-			item.location['z'] = 1;
-			const pileInZOrder = material.location((loc) => loc.x === x).sort((item) => item.location['z']!);
-			for (const item of pileInZOrder.getItems()) {
-				const itemZ = item.location['z'];
-				if (itemZ !== undefined && itemZ >= 1) {
-					item.location['z']!++;
-				}
-			}
-		}
-	}
+        if (material.location((loc) => loc.x === x).length !== 0) {
+            //we’re adding a card under the first one, which never moves. Under this first one, it’s a regular pile
+            //so this card becomes the first of the pile under, and everything in this pile needs to be shifted
+            item.location['z'] = 1
+            const pileInZOrder = material.location((loc) => loc.x === x).sort((item) => item.location['z']!)
+            for (const item of pileInZOrder.getItems()) {
+                const itemZ = item.location['z']
+                if (itemZ !== undefined && itemZ >= 1) {
+                    item.location['z']!++
+                }
+            }
+        }
+    }
 
-	/**
-	 *
-	 * @param material contains all the elements at the same place, including the item to be removed
-	 * @param item
-	 * @returns
-	 */
-	/* When an item is removed, if  it was the only one in its pile, items after it are shifted minus.
-		If it wasn’t the only one, then all remaining elements in its pile are shifted and reranged from 0.
-	*/
-	removeItem(material: Material<P, M, L>, item: MaterialItem<P, L>): void {
-		const x = item.location[this.axis];
-		if (x === undefined) return;
+    /**
+     *
+     * @param material contains all the elements at the same place, including the item to be removed
+     * @param item
+     * @returns
+     */
+    /* When an item is removed, if  it was the only one in its pile, items after it are shifted minus.
+     * If it wasn’t the only one, then all remaining elements in its pile are shifted and reranged from 0.
+     */
+    removeItem(material: Material<P, M, L>, item: MaterialItem<P, L>): void {
+        const x = item.location[this.axis]
+        if (x === undefined) return
 
-		const remainingItemsInPile = material.location((loc) => loc.x === x && loc.z !== item.location.z).sort((item) => item.location['z']!);
-		if (remainingItemsInPile.length > 0) {
-			for (const [i, item] of remainingItemsInPile.getItems().entries()) {
-				item.location['z'] = i;
-			}
-		} else {
-			for (const item of material.getItems()) {
-				const itemX = item.location[this.axis];
-				if (itemX !== undefined && itemX > x) {
-					console.log(item.id, item.location[this.axis],"->",item.location[this.axis]!-1)
-					item.location[this.axis]!--;
-				}
-			}
-		}
-	}
+        const remainingItemsInPile = material.location((loc) => loc.x === x && loc.z !== item.location.z).sort((item) => item.location['z']!)
+        if (remainingItemsInPile.length > 0) {
+            for (const [i, item] of remainingItemsInPile.getItems().entries()) {
+                item.location['z'] = i
+            }
+        } else {
+            for (const item of material.getItems()) {
+                const itemX = item.location[this.axis]
+                if (itemX !== undefined && itemX > x) {
+                    console.log(item.id, item.location[this.axis], '->', item.location[this.axis]! - 1)
+                    item.location[this.axis]!--
+                }
+            }
+        }
+    }
 }
