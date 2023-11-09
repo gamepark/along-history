2f10a27d7c54e7e093bc352382dcb8df92236d88
Romain Fromi <romain.fromi@gmail.com>
2023-11-09 16:10:08 +0100

    Tiger cost 2 more with Woodlands

--- rules/src/material/cards/prehistory/Tiger.ts
+++ rules/src/material/cards/prehistory/Tiger.ts
@@ -4,6 +4,7 @@ import { CardInfo } from '../CardInfo'
 import { CardType } from '../CardType'
 import { ownCard } from '../effects/conditions/OwnCardsCondition'
 import { discard } from '../effects/DiscardEffect'
+import { populationDiscount } from '../effects/DiscountEffect'
 import { losePopulation } from '../effects/LosePopulationEffect'
 
 export const Tiger: CardInfo = {
@@ -12,5 +13,5 @@ export const Tiger: CardInfo = {
   resourcesCost: [Resource.Strength],
   victoryPoints: 2,
   bonus: [],
-  effects: [discard(ownCard(Card.TheFire)), losePopulation()]
+  effects: [discard(ownCard(Card.TheFire)), losePopulation(), populationDiscount(-2, ownCard(Card.Woodlands))]
 }
