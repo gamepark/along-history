9ba8461a2c72017b8ee15bc66081581e212dca94
Romain Fromi <romain.fromi@gmail.com>
2023-11-10 10:23:33 +0100

    Fix EarthquakeFailureRule

--- rules/src/rules/calamities/EarthquakeFailureRule.ts
+++ rules/src/rules/calamities/EarthquakeFailureRule.ts
@@ -1,8 +1,16 @@
+import { CardId } from '../../material/cards/CardId'
+import { CardsInfo } from '../../material/cards/CardsInfo'
 import { LocationType } from '../../material/LocationType'
 import { MaterialType } from '../../material/MaterialType'
 import { LoseCardRule } from './LoseCardRule'
 
 export class EarthquakeFailureRule extends LoseCardRule {
+  getCardsToLose() {
+    const cards = super.getCardsToLose()
+    const bonusCards = cards.id<CardId>(id => CardsInfo[id.front].bonus.length > 0)
+    return bonusCards.length > 0 ? bonusCards : cards
+  }
+
   getEndRuleMoves() {
     return [
       ...this.material(MaterialType.Card).location(LocationType.EventArea).player(this.player).moveItems({ type: LocationType.Discard }),
