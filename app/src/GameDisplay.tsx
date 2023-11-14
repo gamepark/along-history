fed48ec924251867088cdf0a8c603b88263e7d42
Romain Fromi <romain.fromi@gmail.com>
2023-11-14 08:37:25 +0100

    Fix table size for 5 players

--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -5,7 +5,7 @@ import { PlayerPanels } from './panels/PlayerPanels'
 
 export default function GameDisplay() {
   return <>
-    <GameTable xMin={-50} xMax={50} yMin={-36} yMax={36} collisionAlgorithm={pointerWithin}
+    <GameTable xMin={-52} xMax={52} yMin={-33} yMax={33} collisionAlgorithm={pointerWithin}
                margin={{ top: 7, left: 0, right: 30, bottom: 0 }}/>
     <PlayerPanels/>
   </>
