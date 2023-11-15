953bb0b65c1b96a5afb98e58639101bd83f59a74
Romain Fromi <romain.fromi@gmail.com>
2023-11-15 11:24:52 +0100

    Bugfix: no decay with only 1 non-decayed card

--- rules/src/rules/AchievementsRule.ts
+++ rules/src/rules/AchievementsRule.ts
@@ -178,7 +178,7 @@ export class AchievementsRule extends PlayerTurnRule {
       if (achievementToken.length) {
         moves.push(achievementToken.moveItem({ type: LocationType.PlayerAchievements, player: this.player }))
       }
-      if (this.civilisationCards.length > 1) {
+      if (this.nonDecayCards.length > 1) {
         moves.push(this.rules().startRule(RuleId.Decay))
       } else {
         moves.push(this.rules().startRule(RuleId.UniversalResource))
