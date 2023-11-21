2d1b6559f568699eb02a8000ce9130c554d3eec0
Romain Fromi <romain.fromi@gmail.com>
2023-11-21 14:48:57 +0100

    Display the number of victory points

--- app/src/panels/PlayerPanels.tsx
+++ app/src/panels/PlayerPanels.tsx
@@ -1,15 +1,25 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
+import { faStar } from '@fortawesome/free-solid-svg-icons/faStar'
+import { AlongHistoryRules } from '@gamepark/along-history/AlongHistoryRules'
 import { PlayerColor } from '@gamepark/along-history/PlayerColor'
-import { PlayerPanel, usePlayers } from '@gamepark/react-game'
-import { FC } from 'react'
+import { PlayerPanel, usePlayers, useRules } from '@gamepark/react-game'
+import { PlayerPanelCounter } from './PlayerPanelCounter'
 
-export const PlayerPanels: FC<any> = () => {
-  const players = usePlayers({ sortFromMe: true })
+export const PlayerPanels = () => {
+  const players = usePlayers<PlayerColor>({ sortFromMe: true })
+  const rules = useRules<AlongHistoryRules>()
   return (
     <>
       {players.map((player, index) =>
-        <PlayerPanel key={player.id} playerId={player.id} color={playerColorCode[player.id]} css={panelPosition(index, players.length)}/>
+        <PlayerPanel key={player.id} playerId={player.id} color={playerColorCode[player.id]} css={panelPosition(index, players.length)}>
+          <div css={indicators}>
+            <PlayerPanelCounter
+              width={3}
+              icon={faStar}
+              value={rules?.getScore(player.id)!}/>
+          </div>
+        </PlayerPanel>
       )}
     </>
   )
@@ -19,14 +29,14 @@ const panelPosition = (index: number, players: number) => css`
   right: 1em;
   top: ${panelTop(index, players)}em;
   width: 28em;
-  height: 14em;
+  height: 16em;
 `
 
 const panelTop = (index: number, players: number) => {
   if (players === 2) {
     return index === 0 ? 80 : 13
   } else {
-    return 8.5 + index * 16
+    return 9 + index * 18
   }
 }
 
@@ -37,3 +47,13 @@ export const playerColorCode: Record<PlayerColor, string> = {
   [PlayerColor.Green]: 'green',
   [PlayerColor.Red]: 'red'
 }
+
+const indicators = css`
+  width: 100%;
+  padding: 1em;
+  display: flex;
+  flex-direction: row;
+  position: absolute;
+  top: 6em;
+  flex-wrap: wrap;
+`
