5a9547f88d3e3cab318629d48f96ef082fe0853e
Romain Fromi <romain.fromi@gmail.com>
2023-12-01 17:16:58 +0100

    Add dialog when under attack

--- app/src/headers/PrepareArmyHeader.tsx
+++ app/src/headers/PrepareArmyHeader.tsx
@@ -1,9 +1,11 @@
 /** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
 import { AlongHistoryRules } from '@gamepark/along-history/AlongHistoryRules'
 import { CustomMoveType } from '@gamepark/along-history/rules/CustomMoveType'
 import { Memory } from '@gamepark/along-history/rules/Memory'
-import { PlayMoveButton, useLegalMove, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
+import { PlayMoveButton, RulesDialog, ThemeButton, useLegalMove, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
 import { isCustomMoveType } from '@gamepark/rules-api'
+import { useState } from 'react'
 import { Trans, useTranslation } from 'react-i18next'
 
 export const PrepareArmyHeader = () => {
@@ -21,10 +23,29 @@ const PrepareYourArmyHeader = () => {
   const rules = useRules<AlongHistoryRules>()!
   const playerId = usePlayerId()
   const pass = useLegalMove(isCustomMoveType(CustomMoveType.Pass))
-  return <Trans defaults="header.war.prepare" values={{ bonus: rules.remind(Memory.Strength, playerId) }}>
-    <span/>
-    <PlayMoveButton move={pass}/>
-  </Trans>
+  return <>
+    <Trans defaults="header.war.prepare" values={{ bonus: rules.remind(Memory.Strength, playerId) }}>
+      <span/>
+      <PlayMoveButton move={pass}/>
+    </Trans>
+    {rules.remind(Memory.Defender) === playerId && <AttackedInfoDialog/>}
+  </>
+}
+
+const AttackedInfoDialog = () => {
+  const { t } = useTranslation()
+  const rules = useRules<AlongHistoryRules>()!
+  const [open, setOpen] = useState(true)
+  const attacker = rules.remind(Memory.Attacker)
+  const player = usePlayerName(attacker)
+  const strength = rules.remind(Memory.Strength, attacker)
+  return <RulesDialog open={open} close={() => setOpen(false)}>
+    <div css={dialogCss}>
+      <h2>{t('attacked')}</h2>
+      <p><Trans defaults="attacked.detail" values={{ player, strength }}><strong/></Trans></p>
+      <p><ThemeButton onClick={() => setOpen(false)}>{t('OK')}</ThemeButton></p>
+    </div>
+  </RulesDialog>
 }
 
 const PlayerPreparesArmyHeader = () => {
@@ -45,3 +66,18 @@ const PlayerPreparesArmyHeader = () => {
     return <>{t('header.war', { attacker, defender })}</>
   }
 }
+
+const dialogCss = css`
+  max-width: 40em;
+  margin: 1em 2em;
+  font-size: 3em;
+
+  > h2 {
+    margin-right: 2em;
+  }
+
+  > button {
+    display: block;
+    margin: 1em 0;
+  }
+`
