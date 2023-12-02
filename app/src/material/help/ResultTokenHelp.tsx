71f8834cb8ca5b193f8d5cd4406964990d0b973e
Romain Fromi <romain.fromi@gmail.com>
2023-12-02 15:28:47 +0100

    Add button to use result token

--- app/src/material/help/ResultTokenHelp.tsx
+++ app/src/material/help/ResultTokenHelp.tsx
@@ -1,12 +1,16 @@
 /** @jsxImportSource @emotion/react */
-import { MaterialHelpProps } from '@gamepark/react-game'
+import { MaterialType } from '@gamepark/along-history/material/MaterialType'
+import { MaterialHelpProps, PlayMoveButton, useLegalMove } from '@gamepark/react-game'
+import { isMoveItemType, MaterialMove } from '@gamepark/rules-api'
 import { Trans, useTranslation } from 'react-i18next'
 import { breakSpaces } from '../../styles'
 
-export const ResultTokenHelp = ({ item }: MaterialHelpProps) => {
+export const ResultTokenHelp = ({ item, itemIndex, closeDialog }: MaterialHelpProps) => {
   const { t } = useTranslation()
+  const use = useLegalMove<MaterialMove>(move => isMoveItemType(MaterialType.ResultToken)(move) && move.itemIndex === itemIndex)
   return <>
     <h2>{t('result-token')}</h2>
+    {use && <p><PlayMoveButton move={use} onPlay={closeDialog}>{t('result-token.use')}</PlayMoveButton></p>}
     {item.location?.rotation && <p>{t('result-token.flipped')}</p>}
     <p css={breakSpaces}>
       <Trans defaults="result-token.rules"/>
