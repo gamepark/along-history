c2b65aee90c25328cf7b9ba883c4e8d30589bdc1
Romain Fromi <romain.fromi@gmail.com>
2023-12-07 12:27:57 +0100

    Nice positions for 7 dices on Discard tile

--- app/src/locators/DiscardTileLocator.ts
+++ app/src/locators/DiscardTileLocator.ts
@@ -1,14 +1,41 @@
+import { Age } from '@gamepark/along-history/material/Age'
 import { MaterialType } from '@gamepark/along-history/material/MaterialType'
-import { GridLocator } from '@gamepark/react-game'
+import { Memory } from '@gamepark/along-history/rules/Memory'
+import { ItemLocator, MaterialContext } from '@gamepark/react-game'
+import { Location } from '@gamepark/rules-api'
 import { diceDescription } from '../material/DiceDescription'
 
-class DiscardTileLocator extends GridLocator {
+class DiscardTileLocator extends ItemLocator {
   parentItemType = MaterialType.DiscardTile
-  itemsPerLine = 2
-  itemsGap = { x: 2.5 }
-  linesGap = { y: 2.5 }
-  positionOnParent = { x: 50, y: 50 }
-  coordinates = { x: -1.25, y: -2.5, z: diceDescription.width }
+
+  getPositionOnParent(location: Location, { rules }: MaterialContext) {
+    const x = location.x ?? 0
+    if (rules.remind(Memory.CurrentAge) == Age.Prehistory) {
+      return { x: 30 + 40 * (x % 2), y: 20 + Math.floor(x / 2) * 30 }
+    } else {
+      switch (x) {
+        case 0:
+          return { x: 30, y: 20 }
+        case 1:
+          return { x: 70, y: 20 }
+        case 2:
+          return { x: 15, y: 50 }
+        case 3:
+          return { x: 50, y: 50 }
+        case 4:
+          return { x: 85, y: 50 }
+        case 5:
+          return { x: 30, y: 80 }
+        case 6:
+        default:
+          return { x: 70, y: 80 }
+      }
+    }
+  }
+
+  getPosition() {
+    return { x: 0, y: 0, z: diceDescription.width }
+  }
 }
 
 export const discardTileLocator = new DiscardTileLocator()
