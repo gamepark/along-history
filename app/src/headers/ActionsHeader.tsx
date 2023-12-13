399a8fd4ed6d6f8515bdb2fc1552d85650b25ba7
Romain Fromi <romain.fromi@gmail.com>
2023-12-13 22:14:28 +0100

    Confirmation dialog when player try to pass but can still earn some gold

--- app/src/headers/ActionsHeader.tsx
+++ app/src/headers/ActionsHeader.tsx
@@ -1,31 +1,51 @@
 /** @jsxImportSource @emotion/react */
 import { AlongHistoryRules } from '@gamepark/along-history/AlongHistoryRules'
+import { DiceType } from '@gamepark/along-history/material/Dices'
 import { MaterialType } from '@gamepark/along-history/material/MaterialType'
+import { ActionsRule } from '@gamepark/along-history/rules/ActionsRule'
 import { CustomMoveType } from '@gamepark/along-history/rules/CustomMoveType'
-import { PlayMoveButton, useLegalMove, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
-import { isCustomMoveType } from '@gamepark/rules-api'
+import { PlayMoveButton, useLegalMoves, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
+import { isCustomMoveType, isMoveItemType, MaterialMove } from '@gamepark/rules-api'
 import { Trans, useTranslation } from 'react-i18next'
 
 export const ActionsHeader = () => {
+  const rules = useRules<AlongHistoryRules>()!
+  const activePlayer = rules.game.rule?.player
+  const player = usePlayerId()
+  if (player === activePlayer) {
+    return <MyActionsHeader/>
+  } else {
+    return <OtherPlayerActionsHeader/>
+  }
+}
+
+const MyActionsHeader = () => {
+  const { t } = useTranslation()
+  const rules = useRules<AlongHistoryRules>()!
+  const activePlayer = rules.game.rule?.player
+  const legalMoves = useLegalMoves<MaterialMove>()
+  const pass = legalMoves.find(isCustomMoveType(CustomMoveType.Pass))
+  const isActivePlayerTurn = rules.material(MaterialType.DiscardTile).getItem()?.location.player === activePlayer
+  const canGainGold = new ActionsRule(rules.game).tiltGoldBonusCards.length > 0
+    || legalMoves.some(isMoveItemType(MaterialType.ResultToken)) || rules.material(MaterialType.Dice).player(activePlayer).id(DiceType.Gold).length > 0
+  const confirmation = canGainGold ? { text: t('confirm.miss.gold') } : undefined
+  console.log(canGainGold)
+  if (isActivePlayerTurn) {
+    return <Trans defaults="header.actions.you"><PlayMoveButton move={pass} confirmation={confirmation}/></Trans>
+  } else {
+    return <Trans defaults="header.actions.other.you"><PlayMoveButton move={pass} confirmation={confirmation}/></Trans>
+  }
+}
+
+const OtherPlayerActionsHeader = () => {
   const { t } = useTranslation()
   const rules = useRules<AlongHistoryRules>()!
   const activePlayer = rules.game.rule?.player
   const playerName = usePlayerName(activePlayer)
-  const player = usePlayerId()
-  const pass = useLegalMove(isCustomMoveType(CustomMoveType.Pass))
   const isActivePlayerTurn = rules.material(MaterialType.DiscardTile).getItem()?.location.player === activePlayer
-
-  if (player === activePlayer) {
-    if (isActivePlayerTurn) {
-      return <Trans defaults="header.actions.you"><PlayMoveButton move={pass}/></Trans>
-    } else {
-      return <Trans defaults="header.actions.other.you"><PlayMoveButton move={pass}/></Trans>
-    }
+  if (isActivePlayerTurn) {
+    return <>{t('header.actions', { player: playerName })}</>
   } else {
-    if (isActivePlayerTurn) {
-      return <>{t('header.actions', { player: playerName })}</>
-    } else {
-      return <>{t('header.actions.other', { player: playerName })}</>
-    }
+    return <>{t('header.actions.other', { player: playerName })}</>
   }
 }
