d0857a53740e17a5ba67af355b36e1113cec5723
Romain Fromi <romain.fromi@gmail.com>
2023-12-13 22:38:39 +0100

    Do not leave gaps after refreshing the event line if player had 4 cards and did not build the fourth card

--- rules/src/rules/NewEventsRule.ts
+++ rules/src/rules/NewEventsRule.ts
@@ -5,9 +5,14 @@ import { RuleId } from './RuleId'
 
 export class NewEventsRule extends PlayerTurnRule {
   onRuleStart() {
-    const eventCards = this.material(MaterialType.Card).location(LocationType.EventArea).player(this.player).length
+    const eventCards = this.material(MaterialType.Card).location(LocationType.EventArea).player(this.player).getItems()
     const deck = this.material(MaterialType.Card).location(LocationType.Deck).deck()
-    const moves: MaterialMove[] = deck.deal({ type: LocationType.EventArea, player: this.player }, 3 - eventCards)
+    const moves: MaterialMove[] = deck.deal({ type: LocationType.EventArea, player: this.player }, 3 - eventCards.length)
+    for (const eventCard of eventCards) {
+      if (eventCard.location.x! > 2) {
+        eventCard.location.x = [0, 1, 2].find(x => !eventCards.some(card => card.location.x === x))
+      }
+    }
     moves.push(this.rules().startRule(RuleId.Achievements))
     return moves
   }
