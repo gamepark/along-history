1a185031eb65908f33c2c5df4bae5768a91f0f4a
Romain Fromi <romain.fromi@gmail.com>
2023-12-14 12:50:14 +0100

    Bugfix miss gold confirm

--- app/src/headers/ActionsHeader.tsx
+++ app/src/headers/ActionsHeader.tsx
@@ -1,11 +1,12 @@
 /** @jsxImportSource @emotion/react */
 import { AlongHistoryRules } from '@gamepark/along-history/AlongHistoryRules'
 import { DiceType } from '@gamepark/along-history/material/Dices'
+import { isGold } from '@gamepark/along-history/material/DiceSymbol'
 import { MaterialType } from '@gamepark/along-history/material/MaterialType'
 import { ActionsRule } from '@gamepark/along-history/rules/ActionsRule'
 import { CustomMoveType } from '@gamepark/along-history/rules/CustomMoveType'
-import { PlayMoveButton, useLegalMoves, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
-import { isCustomMoveType, isMoveItemType, MaterialMove } from '@gamepark/rules-api'
+import { PlayMoveButton, useLegalMove, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
+import { isCustomMoveType } from '@gamepark/rules-api'
 import { Trans, useTranslation } from 'react-i18next'
 
 export const ActionsHeader = () => {
@@ -23,13 +24,12 @@ const MyActionsHeader = () => {
   const { t } = useTranslation()
   const rules = useRules<AlongHistoryRules>()!
   const activePlayer = rules.game.rule?.player
-  const legalMoves = useLegalMoves<MaterialMove>()
-  const pass = legalMoves.find(isCustomMoveType(CustomMoveType.Pass))
+  const pass = useLegalMove(isCustomMoveType(CustomMoveType.Pass))
   const isActivePlayerTurn = rules.material(MaterialType.DiscardTile).getItem()?.location.player === activePlayer
   const canGainGold = new ActionsRule(rules.game).tiltGoldBonusCards.length > 0
-    || legalMoves.some(isMoveItemType(MaterialType.ResultToken)) || rules.material(MaterialType.Dice).player(activePlayer).id(DiceType.Gold).length > 0
+    || rules.material(MaterialType.ResultToken).player(activePlayer).id(isGold).rotation(undefined).length > 0
+    || rules.material(MaterialType.Dice).player(activePlayer).id(DiceType.Gold).length > 0
   const confirmation = canGainGold ? { text: t('confirm.miss.gold') } : undefined
-  console.log(canGainGold)
   if (isActivePlayerTurn) {
     return <Trans defaults="header.actions.you"><PlayMoveButton move={pass} confirmation={confirmation}/></Trans>
   } else {
