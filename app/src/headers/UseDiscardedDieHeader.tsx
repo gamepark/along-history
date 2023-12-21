24486a83dc1cf218992443e4138341090784b470
Romain Fromi <romain.fromi@gmail.com>
2023-12-21 15:55:15 +0100

    Improve text when last die is discarded

--- app/src/headers/UseDiscardedDieHeader.tsx
+++ app/src/headers/UseDiscardedDieHeader.tsx
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { AlongHistoryRules } from '@gamepark/along-history/AlongHistoryRules'
+import { MaterialType } from '@gamepark/along-history/material/MaterialType'
 import { usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
 import { useTranslation } from 'react-i18next'
 
@@ -9,10 +10,11 @@ export const UseDiscardedDieHeader = () => {
   const activePlayer = rules.game.rule?.player
   const playerName = usePlayerName(activePlayer)
   const player = usePlayerId()
+  const canReroll = rules.material(MaterialType.Dice).player(activePlayer).length > 0
 
   if (player === activePlayer) {
-    return <>{t('header.use-die.you')}</>
+    return <>{t(canReroll ? 'header.use-die.you' : 'header.trade.you')}</>
   } else {
-    return <>{t('header.use-die', { player: playerName })}</>
+    return <>{t(canReroll ? 'header.use-die' : 'header.trade', { player: playerName })}</>
   }
 }
