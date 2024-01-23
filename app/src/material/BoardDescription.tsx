ec1950d4ffa5270f97c1dc83c7f357eaa6358f62
Romain Fromi <romain.fromi@gmail.com>
2024-01-23 15:42:17 +0100

    Make BoardDescription.getFrontId independent of the game

--- app/src/material/BoardDescription.tsx
+++ app/src/material/BoardDescription.tsx
@@ -2,20 +2,19 @@ import { AchievementBoard, AchievementBoardLocations } from '@gamepark/along-his
 import { LocationType } from '@gamepark/along-history/material/LocationType'
 import { Memory } from '@gamepark/along-history/rules/Memory'
 import { BoardDescription, MaterialContext } from '@gamepark/react-game'
+import { MaterialItem } from '@gamepark/rules-api'
 import BoardBack from '../images/board/BoardBack.jpg'
 import BoardFront from '../images/board/BoardFront.jpg'
 import { BoardHelp } from './help/BoardHelp'
 
 class AlongHistoryBoardDescription extends BoardDescription {
-  images = {
-    [AchievementBoard.Front]: BoardFront,
-    [AchievementBoard.Back]: BoardBack
-  }
+  image = BoardFront
+  backImage = BoardBack
   width = 41.826
   height = 18.78
 
-  protected getFrontId(_itemId: never, { rules }: MaterialContext): AchievementBoard {
-    return rules.remind<AchievementBoard | undefined>(Memory.Board) ?? AchievementBoard.Front
+  isFlipped(_item: Partial<MaterialItem>, { rules }: MaterialContext) {
+    return rules.remind<AchievementBoard | undefined>(Memory.Board) === AchievementBoard.Back
   }
 
   staticItem = { location: { type: LocationType.Table } }
